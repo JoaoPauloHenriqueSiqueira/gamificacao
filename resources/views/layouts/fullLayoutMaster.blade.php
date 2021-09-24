b88ae7c04a2d45dbb55f3cb057746660fe38d208
João Siqueira <joao.henriquesiqueirati@gmail.com>
2021-09-24 15:43:06 -0300

    Correcao nome pagina

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -29,7 +29,7 @@ License: You must have a valid license purchased only from themeforest(the above
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title') | Materialize - Material Design Admin Template</title>
+  <title>@yield('title') | ExibeTV</title>
   <link rel="apple-touch-icon" href="../../../images/favicon/apple-touch-icon-152x152.png">
   <link rel="shortcut icon" type="image/x-icon" href="../../../images/favicon/favicon-32x32.png">
 
