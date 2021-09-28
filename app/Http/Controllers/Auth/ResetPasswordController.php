7af0f429d40bbaffe9726464c1e1221b4312d907
João Siqueira <joao.henriquesiqueirati@gmail.com>
2021-09-28 18:54:19 -0300

    Correcao troca senha

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -28,7 +28,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/page-blank';
+    protected $redirectTo = '/dashboard/admin';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/page-blank';
+    protected $redirectTo = '/dashboard/admin';
 
     /**
      * Create a new controller instance.
